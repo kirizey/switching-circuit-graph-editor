b736037377f266083d91a9294b7902aef7668390
Kirill Dolgikh <kirill.dolgikh.evosoft@gmail.com>
2020-11-29 12:03:03 +0700

    ]e]e

--- src/components/ComponentsGraph.jsx
+++ src/components/ComponentsGraph.jsx
@@ -1,34 +1,41 @@
 import React, { useState } from 'react';
 import { Graph } from 'react-d3-graph';
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-}
-
-const myConfig = {
-  automaticRearrangeAfterDropNode: true,
-  nodeHighlightBehavior: true,
-  node: {
-    color: 'lightgreen',
-    size: 600,
-    highlightStrokeColor: 'blue',
-    fontSize: 16,
-    highlightFontSize: 22,
-    fontColor: 'white',
-  },
-  link: {
-    highlightColor: 'lightblue',
-    fontSize: 16,
-    highlightFontSize: 22,
-  },
-};
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+// }
 
 export default function ComponentsGraph({ plate, renderMatrix }) {
   if (!plate || !renderMatrix) {
     return null;
   }
+  const myConfig = {
+    automaticRearrangeAfterDropNode: true,
+    height: 400,
+    node: {
+      color: 'lightgreen',
+      size: 1800,
+      highlightStrokeColor: 'blue',
+      fontSize: 20,
+      highlightFontSize: 20,
+      fontColor: 'white',
+      labelPosition: 'center',
+    },
+    link: {
+      highlightColor: 'lightblue',
+      fontSize: 20,
+      highlightFontSize: 20,
+    },
+    d3: {
+      alphaTarget: 0.3,
+      gravity: -201,
+      linkLength: 300,
+      linkStrength: 1,
+      disableLinkForce: false,
+    },
+  };
 
   const allComponents = plate.map((node) => node.components).flat();
   const components = [...new Set(allComponents.map((component) => component.name))].map((name) => ({
@@ -46,36 +53,32 @@ export default function ComponentsGraph({ plate, renderMatrix }) {
     }
   });
 
-  const links = []
-  
+  const links = [];
+
   for (let i = renderMatrix._size[0]; i > 1; i--) {
-    for (let j = i; j >  1; j--) {
-      if (renderMatrix._data[i][j-1]>0) {
-        const s = renderMatrix._data[0][j-1];
+    for (let j = i; j > 1; j--) {
+      if (renderMatrix._data[i][j - 1] > 0) {
+        const s = renderMatrix._data[0][j - 1];
         const t = renderMatrix._data[i][0];
 
-        links.push({source: s, target: t})
+        links.push({ source: s, target: t });
       }
-    } 
+    }
   }
-
-  console.log(links);
-  
 
   const graph = {
     nodes: components?.map((c, index) => ({
       id: c.name,
-      x: getRandomInt(1, 800),
-      y: getRandomInt(1, 800),
+      // x: getRandomInt(1, 800),
+      // y: getRandomInt(1, 800),
       symbolType: 'square',
     })),
     links,
   };
 
-
   const renderGraph = (
     <Graph
-      id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+      id="graph-id"
       data={graph}
       config={myConfig}
     />
